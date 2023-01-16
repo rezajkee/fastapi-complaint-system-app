eeb6228239dcfe2b15d62f54697cd46b0240158c
Anton Shefer <rezajkee@mail.ru>
2023-01-16 16:37:29 +0500

    Implement getting users list and get an user by email

---
+++ resources/user.py
@@ -0,0 +1,15 @@
+from typing import Optional
+
+from fastapi import APIRouter, Depends
+
+from managers.auth import is_admin, oauth2_scheme
+from managers.user import UserManager
+
+router = APIRouter(tags=["Users"])
+
+
+@router.get("/users/", dependencies=[Depends(oauth2_scheme), Depends(is_admin)])
+async def get_users(email: Optional[str] = None):
+    if email:
+        return await UserManager.get_user_by_email(email)
+    return await UserManager.get_all_users()
